f315f27e1d17ed2dca84f2dbe1693a689cc32645
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-04-09 19:27:37 -0300

    regular button component

---
+++ resources/js/components/button/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+  // props
+  export let text = 'button';
+  export let type = 'button';
+</script>
+
+<button class="btn btn-primary capitalize" type="{type}">{text}</button>
+
+<style>
+  /* your styles go here */
+</style>

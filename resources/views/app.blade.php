e2f7abec52619c71c5fae3b42af40407a6af815c
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-04-13 06:36:16 -0300

    fix favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    <link rel="shortcut icon" href="/assets/favicon.svg">
+    <link rel="shortcut icon" href="assets/favicon.svg">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
